c8265431ceac3258000ce0928cfce11b2fbbda01
Your Name <dungpro520@gmail.com>
2024-05-08 13:06:06 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>FullName: Nguyễn Tấn Dũng </p>
+        <p>Class: SE17D08</p>
+        <p>School: FPT UNIVERSITY</p>
+        <p>Description: I am 21 years old.</p>
       </header>
     </div>
   );
